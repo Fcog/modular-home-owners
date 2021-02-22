19f0c72b681fe9df2537091ed023fc576d473689
Francisco Giraldo <fgiraldo@wearenolte.com>
2021-02-22 11:28:40 -0500

    Removed dev settings

--- mhoapp/settings/development.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(fa$5!a)@j!d(wjg+#3$#=o!8r74feue3xy*u9br_p%by$x7do'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
