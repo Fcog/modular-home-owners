15eea16eb0b9fbbc0350a5febe9ab45db088774b
Francisco Giraldo <fgiraldo@wearenolte.com>
2021-02-23 11:31:52 -0500

    S3 configs

--- mhoapp/settings.py
+++ mhoapp/settings.py
@@ -184,8 +184,12 @@ else:
     }
 
     AWS_LOCATION = 'static'
-    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mhoapp/static'),
