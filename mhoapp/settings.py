5fa70e6f5772baad07662a57173d7802bd399c55
Francisco Giraldo <fgiraldo@wearenolte.com>
2021-02-23 11:55:39 -0500

    django-storage settings

--- mhoapp/settings.py
+++ mhoapp/settings.py
@@ -185,8 +185,8 @@ else:
 
     AWS_LOCATION = 'static'
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
