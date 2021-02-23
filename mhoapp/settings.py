fb731cb18866daf0658926f4167e1d6b52382bbb
Francisco Giraldo <fgiraldo@wearenolte.com>
2021-02-23 09:50:54 -0500

    Added whitenoise config

--- mhoapp/settings.py
+++ mhoapp/settings.py
@@ -159,6 +159,10 @@ else:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    'mhoapp/static',
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
