89953b7fdc7e1f9276bd7d8acd4b83cbb10a411a
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2021-04-28 10:51:51 -0500

    Removed float zero from baths

--- mhoapp/theme/templatetags/custom_filters.py
+++ mhoapp/theme/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 
+import math
 from django import template
 from django.contrib.humanize.templatetags.humanize import intcomma
 
@@ -8,3 +9,10 @@ def currency(dollars):
     return "${:,}".format(dollars)
 
 register.filter('currency', currency)
+
+def truncate_float(value):
+    if value.is_integer():
+        return math.trunc(value)
+    return value
+
+register.filter('truncate_float', truncate_float)    
