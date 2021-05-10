81c6df906f0274c18cea5a090122d377eea72c2f
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2021-05-09 19:20:26 -0500

    Navbar blue style added

--- mhoapp/theme/static_src/src/index.js
+++ mhoapp/theme/static_src/src/index.js
@@ -20,27 +20,31 @@ document.addEventListener( 'DOMContentLoaded', () => {
     /**
      * Main menu mobile dropdown toggling.
      */
-    menuMobileDropdown(
-        '.luxbar-menu .dropdown', 
-        '.luxbar-item-a-first-level', 
-        'ul', 
-        (menuItem, submenu) => { 
-            submenu.classList.toggle('block')
-            menuItem.classList.toggle('opened')
-        }
-    )
+    if ( window.innerWidth < 768 ) {
+        menuMobileDropdown(
+            '.luxbar-menu .dropdown', 
+            '.luxbar-item-a-first-level', 
+            'ul', 
+            (menuItem, submenu) => { 
+                submenu.classList.toggle('block')
+                menuItem.classList.toggle('opened')
+            }
+        )
+    }
 
     /**
      * Footer menu mobile dropdown toggling.
      */    
-    menuMobileDropdown(
-        '.js-menu-vertical', 
-        '.js-menu-vertical-parent-menu', 
-        '.js-menu-vertical-submenu',
-        (menuItem, submenu) => { 
-            submenu.classList.toggle('hidden')
-            menuItem.classList.toggle('opened')
-            menuItem.classList.toggle('icon-rotate')
-        }
-    )
+    if ( window.innerWidth < 768 ) {
+        menuMobileDropdown(
+            '.js-menu-vertical', 
+            '.js-menu-vertical-parent-menu', 
+            '.js-menu-vertical-submenu',
+            (menuItem, submenu) => { 
+                submenu.classList.toggle('hidden')
+                menuItem.classList.toggle('opened')
+                menuItem.classList.toggle('icon-rotate')
+            }
+        )
+    }
 })
