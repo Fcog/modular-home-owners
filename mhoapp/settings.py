7c4d7788ac59338c15987167998cfd457a80f9ef
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2021-06-09 11:19:25 -0500

    Static files path changed

--- mhoapp/settings.py
+++ mhoapp/settings.py
@@ -290,6 +290,7 @@ else:
     # Use AWS S3 storage
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_S3_ENDPOINT_URL = os.environ['AWS_S3_ENDPOINT_URL']
     AWS_DEFAULT_ACL = 'public-read'
 
