1cc452d6e7a4817296ebf065af88200f19f3d482
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2021-09-18 21:23:55 +0200

    Added archive page

--- mhoapp/partners/models/PartnersIndexPage.py
+++ mhoapp/partners/models/PartnersIndexPage.py
@@ -2,7 +2,7 @@ from wagtail.core.models import Page
 
 
 class PartnersIndexPage(Page):
-    template = 'patterns/templates/flexible/two-col.html'
+    template = 'patterns/templates/archive/archive.html'
 
     # Parent page / subpage type rules
     subpage_types = ['PartnerTypePage']
